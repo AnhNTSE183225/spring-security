3884d8a46a614d2179d5a63399a2758912086301
AnhNT <anhntse183225@fpt.edu.vn>
2025-01-21 23:37:20 +0700

    test redirection

--- src/main/java/com/theanh/securityproject/configuration/JwtFilter.java
+++ src/main/java/com/theanh/securityproject/configuration/JwtFilter.java
@@ -31,6 +31,10 @@ public class JwtFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+        if(request.getServletPath().equals("/")) {
+            response.sendRedirect(request.getContextPath() + "/swagger-ui/index.html");
+            return;
+        }
         if (request.getServletPath().contains("/api/v1/auth")) {
             filterChain.doFilter(request, response);
             return;
