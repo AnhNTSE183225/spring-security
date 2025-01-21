5bfe09eaf7e8b114a7a9a697a6f9e199dc659a60
AnhNT <anhntse183225@fpt.edu.vn>
2025-01-21 21:20:37 +0700

    Add OpenAPI config

---
+++ src/main/java/com/theanh/securityproject/configuration/OpenAPIConfig.java
@@ -0,0 +1,33 @@
+package com.theanh.securityproject.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Spring Security API"))
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList("The Anh")
+                ).components(
+                        new Components().addSecuritySchemes(
+                                "The Anh Authentication Service",
+                                new SecurityScheme()
+                                        .name("Bearer")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+
+}
