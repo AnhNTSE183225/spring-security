6df1de897719c87cc67186e40dac768ffce6e7a6
AnhNT <theanh08042004@gmail.com>
2025-02-26 08:38:50 +0700

    D

--- src/main/java/com/theanh/securityproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/theanh/securityproject/exception/GlobalExceptionHandler.java
@@ -2,13 +2,29 @@ package com.theanh.securityproject.exception;
 
 import com.theanh.securityproject.dto.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ApiResponse<String>> runtimeException(AuthorizationDeniedException e) {
+        return ResponseEntity
+                .status(FORBIDDEN)
+                .body(
+                        ApiResponse.<String>builder()
+                                .statusCode(FORBIDDEN.value())
+                                .success(false)
+                                .message(e.getMessage())
+                                .data(null)
+                                .build()
+                );
+    }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ApiResponse<String>> runtimeException(RuntimeException e) {
@@ -24,5 +40,6 @@ public class GlobalExceptionHandler {
                                 .build()
                 );
     }
+
 
 }
